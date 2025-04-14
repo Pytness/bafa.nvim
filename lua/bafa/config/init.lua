82c49292140e54e3efc4f95a4846e09b6ff64cab
Fernando Vega <pytness@gmail.com>
2025-04-14 16:55:38 +0200

    Add documentation and SortingAlgorithm type

--- lua/bafa/config/init.lua
+++ lua/bafa/config/init.lua
@@ -1,5 +1,30 @@
+--- @class bafa.config.sorting_algorithm_enum
+--- @field LAST_USED string
+--- @field NAME string
+--- @field MODIFIED string
+
+--- @class bafa.config.config
+--- @field title string The title of the Bafa menu
+--- @field title_pos string The position of the title
+--- @field relative string The relative position of the Bafa menu
+--- @field border string The border style of the Bafa menu
+--- @field style string The style of the Bafa menu
+--- @field diagnostics boolean Whether to show diagnostics
+--- @field icons boolean Whether to show icons
+--- @field keymaps table<string> | nil The keymaps for the Bafa menu
+--- @field noop_keys table<string> | nil The keys to ignore in the Bafa menu
+--- @field sorting_algorithm string The sorting algorithm for the Bafa menu
+
 local M = {}
 
+--- @type bafa.config.sorting_algorithm_enum
+M.SortingAlgorithm = {
+  LAST_USED = "last_used",
+  NAME = "name",
+  MODIFIED = "modified",
+}
+
+--- @type bafa.config.config
 M.defaults = {
   title = "Bafa",
   title_pos = "center",
@@ -12,18 +37,31 @@ M.defaults = {
 
   keymaps = nil,
   noop_keys = nil,
+
+  sorting_algorithm = M.SortingAlgorithm.LAST_USED,
 }
 
 M.options = M.defaults
 
+--- Setup the configuration
+---
+--- @param config bafa.config.config The configuration table
+--- @return nil
 function M.setup(config)
   M.options = vim.tbl_deep_extend("force", M.defaults, config or {})
 end
 
+--- Set the configuration
+---
+--- @param config bafa.config.config The configuration table
+--- @return nil
 function M.set(config)
   M.options = vim.tbl_deep_extend("force", M.options, config or {})
 end
 
+--- Get the current configuration
+---
+--- @return bafa.config.config # The configuration table
 function M.get()
   return M.options
 end
