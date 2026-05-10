fdb6bb08b0f0e126f51ad2e080f2541fd8ae07aa
Fernando Vega <pytness@gmail.com>
2026-05-11 01:44:42 +0200

    fix: BufModifiedSet was droped
    #25fcd596890fe125d0d8c618a069ebda47c9f1a1

--- lua/bafa/utils/autocmds.lua
+++ lua/bafa/utils/autocmds.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.set_defaults(bufnr)
   vim.cmd(string.format("autocmd BufWriteCmd <buffer=%s> lua require('bafa.ui').on_menu_save()", bufnr))
-  vim.cmd(string.format("autocmd BufModifiedSet <buffer=%s> set nomodified", bufnr))
+  vim.cmd(string.format("autocmd Optionset nomodified <buffer=%s>", bufnr))
   vim.cmd("autocmd BufLeave <buffer> ++nested ++once silent lua require('bafa.ui').toggle()")
 end
 
